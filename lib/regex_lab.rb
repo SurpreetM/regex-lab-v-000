8dadedfcedc7ef2f01e78d3658b47248357aa484
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-07-14 20:06:46 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,11 +15,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z].+[.!?]$/) == nil
+  if text.match(/^[A-Z].+[.,!?]/) == nil
     false
   else true
   end
-
 end
 
 def valid_phone_number?(phone)
